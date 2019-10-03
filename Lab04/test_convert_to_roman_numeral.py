92413f08d124eab44f0cede9b4f83e4c0ff74dcb
Jkcadee <janellesertyankwok@gmail.com>
2019-10-03 13:28:08 -0700

    Split up self.asserts into different functions

--- Lab04/test_convert_to_roman_numeral.py
+++ Lab04/test_convert_to_roman_numeral.py
@@ -3,23 +3,57 @@ from A01166243_1510_labs.Lab04.roman_numbers import convert_to_roman_numeral
 
 
 class TestConvertToRomanNumeral(TestCase):
-    def test_convert_to_roman_numeral(self):
+    def test_convert_to_roman_numeral_1(self):
         self.assertEqual('I', convert_to_roman_numeral(1))
+
+    def test_convert_to_roman_numeral_10000(self):
         self.assertEqual('MMMMMMMMMM', convert_to_roman_numeral(10000))
+
+    def test_convert_to_roman_numeral_4(self):
         self.assertEqual('IV', convert_to_roman_numeral(4))
+
+    def test_convert_to_roman_numeral_9(self):
         self.assertEqual('IX', convert_to_roman_numeral(9))
+
+    def test_convert_to_roman_numeral_5(self):
         self.assertEqual('V', convert_to_roman_numeral(5))
+
+    def test_convert_to_roman_numeral_6(self):
         self.assertEqual('VI', convert_to_roman_numeral(6))
+
+    def test_convert_to_roman_numeral_10(self):
         self.assertEqual('X', convert_to_roman_numeral(10))
+
+    def test_convert_to_roman_numeral_20(self):
         self.assertEqual('XX', convert_to_roman_numeral(20))
+
+    def test_convert_to_roman_numeral_40(self):
         self.assertEqual('XL', convert_to_roman_numeral(40))
+
+    def test_convert_to_roman_numeral_50(self):
         self.assertEqual('L', convert_to_roman_numeral(50))
+
+    def test_convert_to_roman_numeral_60(self):
         self.assertEqual('LX', convert_to_roman_numeral(60))
+
+    def test_convert_to_roman_numeral_90(self):
         self.assertEqual('XC', convert_to_roman_numeral(90))
+
+    def test_convert_to_roman_numeral_100(self):
         self.assertEqual('C', convert_to_roman_numeral(100))
+
+    def test_convert_to_roman_numeral_400(self):
         self.assertEqual('CD', convert_to_roman_numeral(400))
+
+    def test_convert_to_roman_numeral_500(self):
         self.assertEqual('D', convert_to_roman_numeral(500))
+
+    def test_convert_to_roman_numeral_900(self):
         self.assertEqual('CM', convert_to_roman_numeral(900))
+
+    def test_convert_to_roman_numeral_1000(self):
         self.assertEqual('M', convert_to_roman_numeral(1000))
+
+    def test_convert_to_roman_numeral_5000(self):
         self.assertEqual('MMMMM', convert_to_roman_numeral(5000))
 
--- Lab04/test_time_calculator.py
+++ Lab04/test_time_calculator.py
@@ -3,12 +3,27 @@ from A01166243_1510_labs.Lab04.time_calculator import time_calculator
 
 
 class TestTimeCalculator(TestCase):
-    def test_time_calculator(self):
+    def test_time_calculator_0(self):
         self.assertEqual('0 days, 0 hours, 0 minutes, 0 seconds.', time_calculator(0))
+
+    def test_time_calculator_59(self):
         self.assertEqual('0 days, 0 hours, 0 minutes, 59 seconds.', time_calculator(59))
+
+    def test_time_calculator_60(self):
         self.assertEqual('0 days, 0 hours, 1 minutes, 0 seconds.', time_calculator(60))
+
+    def test_time_calculator_3599(self):
         self.assertEqual('0 days, 0 hours, 59 minutes, 59 seconds.', time_calculator(3599))
+
+    def test_time_calculator_3600(self):
         self.assertEqual('0 days, 1 hours, 0 minutes, 0 seconds.', time_calculator(3600))
+
+    def test_time_calculator_86399(self):
         self.assertEqual('0 days, 23 hours, 59 minutes, 59 seconds.', time_calculator(86399))
+
+    def test_time_calculator_86400(self):
         self.assertEqual('1 days, 0 hours, 0 minutes, 0 seconds.', time_calculator(86400))
+
+    def test_time_calculator_86401(self):
         self.assertEqual('1 days, 0 hours, 0 minutes, 1 seconds.', time_calculator(86401))
+
