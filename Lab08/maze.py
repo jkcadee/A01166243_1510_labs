e48771d59740406be22dfa1fcd8da5d598d37a9a
Jkcadee <janellesertyankwok@gmail.com>
2019-11-04 17:21:38 -0800

    added functions to maze.py

--- Lab08/maze.py
+++ Lab08/maze.py
@@ -1,16 +1,15 @@
 def make_board(row_size: int, column_size: int) -> list:
-    space = 'x'
     board = []
     for r in range(row_size):  # Creates a list for each row.
         row = []
         for c in range(column_size):  # Populates the list with a pair of coords for each row.
-            row.append(space)
+            row.append((c, r))
         board.append(row)
     return board
 
 
-def make_character():
-    character = []
+def make_character(x_coord, y_coord):
+    character = [x_coord, y_coord]
     return character
 
 
@@ -19,16 +18,45 @@ def user_choice():
     return move.capitalize()
 
 
-# def move_character(board, character_coords):
+def move_character(character_coords, maximum, pos, direction):
+    if direction == ('N' or 'W'):
+        if character_coords[pos] != 0:
+            character_coords[pos] -= 1
+    elif direction == ('S' or 'E'):
+        if character_coords[pos] != maximum:
+            character_coords[pos] += 1
 
 
-# def validate_move(board, character, direction):
+def validate_move(maximum, character, direction):
+    if direction == ('N' or 'S'):
+        move_character(character, maximum, 0, direction)
+    elif direction == ('E' or 'W'):
+        move_character(character, maximum, 1, direction)
+
+
+def display_board(board, character):
+    for index in range(len(board)):
+        for index_two in range(len(board[index])):
+            current_position = [index, index_two]
+            if current_position == character:
+                if index_two == len(board[index]) - 1:
+                    print('c')
+                else:
+                    print('c', end=" ")
+            else:
+                if index_two == len(board[index]) - 1:
+                    print('x')
+                else:
+                    print('x', end=" ")
 
 
 def game():
+    character_list = make_character(0, 0)
     board = make_board(5, 5)
-    for x in board:
-        print(x)
+    while True:
+        display_board(board, character_list)
+        character_list = validate_move(5, character_list, user_choice())
+
 
 
 def main():
