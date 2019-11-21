4ef605c574f9f691f9982a7e2d26802ef438c351
Jkcadee <janellesertyankwok@gmail.com>
2019-11-21 12:12:32 -0800

    Redid code for question 1

--- Lab10/question_1.py
+++ Lab10/question_1.py
@@ -1,35 +1,6 @@
 import doctest
 
 
-def sieve_multiples(number_list: list) -> list:
-    """
-    Return a list of prime numbers generated from the parameter list.
-
-    :param number_list: List.
-    :precondition: Must be a list with at least one element higher or at 2. Values must be integers. Values must be
-    concurrent.
-    :postcondition: Loop through the list and determine which values are prime numbers. The prime numbers are added to a
-    separate list and that list is returned.
-    :return: The list of generated prime numbers.
-
-    >>> sieve_multiples([2, 3, 4, 5, 6, 7, 8])
-    [2, 3, 5, 7]
-    >>> sieve_multiples([3, 4, 5, 6, 7, 8, 9, 10, 11])
-    [2, 3, 5, 7, 11]
-    """
-    prime_number_list = [2]
-    for x in number_list:
-        copy_prime_number_list = prime_number_list.copy()
-        prime = True
-        for y in copy_prime_number_list:
-            if x % y == 0:
-                prime = False
-                break
-        if prime:
-            prime_number_list.append(x)
-    return prime_number_list
-
-
 def eratothenes(upperbound: int) -> list:
     """
     Return a list of only prime numbers, based on how many elements are inside the generated all_numbers list.
@@ -40,21 +11,26 @@ def eratothenes(upperbound: int) -> list:
     numbers from taken from values from the all_numbers list.
     :return: A list of prime numbers taken from all_numbers list.
 
-    >>> eratothenes(5)
-    [2, 3, 5]
-    >>> eratothenes(1)
-    []
-    >>> eratothenes(10)
-    [2, 3, 5, 7]
-    >>> eratothenes(-1)
-    Wrong input!
+    # >>> eratothenes(5)
+    # [2, 3, 5]
+    # >>> eratothenes(1)
+    # []
+    # >>> eratothenes(10)
+    # [2, 3, 5, 7]
+    # >>> eratothenes(-1)
+    # Wrong input!
     """
     if upperbound <= 0:
-        print("Wrong input!")
-    all_numbers = [i for i in range(0, upperbound + 1)]
-    all_numbers.remove(0)
-    all_numbers.remove(1)
-    return all_numbers if len(all_numbers) == 0 else sieve_multiples(all_numbers)
+        raise ValueError("Invalid Number.")
+    bool_primes_list = [True for i in range(0, upperbound + 1)]
+    bool_primes_list[0] = False
+    bool_primes_list[1] = False
+    for index, value in enumerate(bool_primes_list):
+        if value:
+            for multiple in range(index+index, upperbound + 1, index):
+                bool_primes_list[multiple] = False
+    primes_list = [index for index, value in enumerate(bool_primes_list) if value]
+    return primes_list
 
 
 def main():
@@ -63,4 +39,6 @@ def main():
 
 
 if __name__ == "__main__":
+    main()
     doctest.testmod()
+
