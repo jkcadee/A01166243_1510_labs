d183a201efa1b1ff07de9571305f71d850ca0e16
Jkcadee <janellesertyankwok@gmail.com>
2019-11-28 12:57:59 -0800

    Made timer function

---
+++ Lab11/lab_11 (Kayden and Janelle).py
@@ -0,0 +1,13 @@
+import time
+
+
+def timer(function):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        value = function(*args, **kwargs)
+        end = time.perf_counter()
+        run_time = end - start
+        with open('result.txt', 'a+') as file_object:
+            file_object.write(f"{function.__name__!r}: {run_time:.4f} seconds")
+    return wrapper
+
