be20c01a50d828ba693904a429817acd9964ca65
Jkcadee <janellesertyankwok@gmail.com>
2019-11-28 13:10:00 -0800

    Janelle Turn 1 done

--- Lab11/lab_11 (Kayden and Janelle).py
+++ Lab11/lab_11 (Kayden and Janelle).py
@@ -1,4 +1,5 @@
 import time
+import doctest
 
 
 def timer(function):
@@ -8,7 +9,25 @@ def timer(function):
         end = time.perf_counter()
         run_time = end - start
         with open('result.txt', 'a+') as file_object:
-            file_object.write(f"{function.__name__!r}: {run_time:.4f} seconds")
+            file_object.write(f"{function.__name__!r}: {run_time:.12f} seconds\n")
         return value
     return wrapper
 
+
+def factorial_iterative(upper):
+    """
+    Return the factorial of between 1 and the upperbound number.
+
+    :param upper: Integer.
+    :precondition: Upper must be positive.
+    :return: The factorial between 1 and upperbound.
+    """
+    result = 1
+    i = 2
+    while i <= upper:
+        result *= i
+        i += 1
+    return result
+
+
+# def factorial_recursive():
